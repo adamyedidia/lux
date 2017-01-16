165e389072bb91bcb3560de407c363c36d13463b
Katherine Bouman <klbouman@Katherines-MacBook-Pro-3.local>
2017-01-16 16:36:35 -0500

    fixed weird spacing

--- edgeImaging/edges/estimateAmat.m
+++ edgeImaging/edges/estimateAmat.m
@@ -1,5 +1,5 @@
 addpath(genpath('rectify'));
-    
+
 datafolder = '../data/testvideos/experiment_2';
 %datafolder = '/Users/klbouman/Downloads';
 
@@ -11,11 +11,11 @@ v = VideoReader(moviefile);
 frame1 = double(read(v,1));
 
 if do_rectify == 1
-	v = VideoReader(gridfile);
-	caliImg = read(v,100);
-	[iold, jold, ii, jj, rectified_img] = rectify_image_solve(caliImg);
-	#figure;imagesc(rectified_img./max(rectified_img(:)))
-	frame1 = rectify_image(frame1, iold, jold, ii, jj);
+    v = VideoReader(gridfile);
+    caliImg = read(v,100);
+    [iold, jold, ii, jj, rectified_img] = rectify_image_solve(caliImg);
+    #figure;imagesc(rectified_img./max(rectified_img(:)))
+    frame1 = rectify_image(frame1, iold, jold, ii, jj);
 end
 
 frame1 = blurDnClr(frame1, 3, binomialFilter(5));
@@ -24,37 +24,37 @@ imagesc(frame1(:,:,1));
 corner = ginput(1);
 hold on; plot(corner(1), corner(2), 'ro');
 
-vout = VideoWriter(sprintf('%s/out_red1.MOV', datafolder)); 
-vout.FrameRate = 10; 
-minclip = 0; 
-maxclip = 1; 
+vout = VideoWriter(sprintf('%s/out_red1.MOV', datafolder));
+vout.FrameRate = 10;
+minclip = 0;
+maxclip = 1;
 open(vout)
 
 for n=1:500
-	n
+    n
     
-    	% read the nth frame
-    	framen = double(read(v,n));
-    	if do_rectify == 1
-		framen = rectify_image(framen, iold, jold, ii, jj);
-    	end
-	framen = blurDnClr(framen, 3, binomialFilter(5));
-    	
-	count = 1;
-    	for r = 10:5:30
-        	[rgbq(:,:,:,count), diffs(:,:,:,count)] = gradientAlongCircle(r, 200, framen, corner);
-        	count = count + 1;
-    	end
+    % read the nth frame
+    framen = double(read(v,n));
+    if do_rectify == 1
+        framen = rectify_image(framen, iold, jold, ii, jj);
+    end
+    framen = blurDnClr(framen, 3, binomialFilter(5));
     
-    	%compute the average frame from all the circle differences
-    	outframe(1,:,:) = mean(diffs,4); 
+    count = 1;
+    for r = 10:5:30
+        [rgbq(:,:,:,count), diffs(:,:,:,count)] = gradientAlongCircle(r, 200, framen, corner);
+        count = count + 1;
+    end
     
-    	%write out the video
-    	outframe(outframe<minclip) = minclip;
-    	outframe(outframe>maxclip) = maxclip; 
+    %compute the average frame from all the circle differences
+    outframe(1,:,:) = mean(diffs,4);
     
-    	writeVideo(vout, (repmat(outframe, [100 1]) -minclip)./(maxclip-minclip)); 
+    %write out the video
+    outframe(outframe<minclip) = minclip;
+    outframe(outframe>maxclip) = maxclip;
+    
+    writeVideo(vout, (repmat(outframe, [100 1]) -minclip)./(maxclip-minclip));
 end
 
-close(vout); 
+close(vout);
 

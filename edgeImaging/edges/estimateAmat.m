d6fd29d16657a6215edaf3df8a6b492bd4ffd54b
Vickie Ye <vye1116@gmail.com>
2017-01-16 13:41:47 -0500

    gradient along a circle computation started, not really working; added pyr library mex files to speed up and get rid of all the warnings

--- edgeImaging/edges/estimateAmat.m
+++ edgeImaging/edges/estimateAmat.m
@@ -1,22 +1,12 @@
 datafolder = '../data/testvideos/experiment_2';
-fname = sprintf('%s/dark_IMG_3294.MOV', datafolder);
+fname = sprintf('%s/dark_MovieLines_green1.MOV', datafolder);
 v = VideoReader(fname);
 frame1 = double(readFrame(v));
-frame1 = blurDnClr(blurDnClr(blurDnClr(frame1)));
+frame1 = blurDnClr(frame1, 3, binomialFilter(5));
 imagesc(frame1(:,:,1));
-[corner_x, corner_y] = ginput(1);
+corner = ginput(1);
+hold on; plot(corner(1), corner(2), 'ro');
 
-r = 10;
-nsamples = 200;
-angles = linspace(0, pi/2, nsamples);
-xq = corner_x + r * cos(angles);
-yq = corner_y + r * sin(angles);
-hold on;
-plot(xq, yq, 'r');
-
-[yy, xx] = ndgrid(1:size(frame1, 1), 1:size(frame1, 2));
-rgbq = zeros([size(angles, 2), size(frame1, 3)]);
-for i = 1:size(rgbq,2)
-    rgbq(:,i) = interp2(xx, yy, frame1(:,:,i), xq, yq);
+for r = 10:5:30
+    gradientAlongCircle(r, 200, frame1, corner);
 end
-

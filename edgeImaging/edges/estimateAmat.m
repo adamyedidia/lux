bf10e191c36608f36d242e2c212a6a0e8331f6f5
Katherine Bouman <klbouman@Katherines-MacBook-Pro-3.local>
2017-01-16 16:13:24 -0500

    make a video from looking at the edge shadow

--- edgeImaging/edges/estimateAmat.m
+++ edgeImaging/edges/estimateAmat.m
@@ -1,12 +1,50 @@
 datafolder = '../data/testvideos/experiment_2';
-fname = sprintf('%s/dark_MovieLines_green1.MOV', datafolder);
+%datafolder = '/Users/klbouman/Downloads';
+
+v = VideoReader(sprintf('%s/calibrationgrid.MOV', datafolder));
+caliImg = read(v,100);
+[iold, jold, ii, jj, rectified_img] = rectify_image_solve(caliImg);
+figure;imagesc(rectified_img./max(rectified_img(:)))
+
+fname = sprintf('%s/dark_MovieLines_red1.MOV', datafolder);
 v = VideoReader(fname);
-frame1 = double(readFrame(v));
+frame1 = double(read(v,1));
+frame1 = rectify_image(frame1, iold, jold, ii, jj);
 frame1 = blurDnClr(frame1, 3, binomialFilter(5));
 imagesc(frame1(:,:,1));
 corner = ginput(1);
 hold on; plot(corner(1), corner(2), 'ro');
 
-for r = 10:5:30
-    gradientAlongCircle(r, 200, frame1, corner);
+vout = VideoWriter(sprintf('%s/out_red1.MOV', datafolder)); 
+vout.FrameRate = 10; 
+minclip = 0; 
+maxclip = 1; 
+open(vout)
+
+for n=1:500
+    n
+    
+    % read the nth frame
+    framen = double(read(v,n));
+    framen = rectify_image(framen, iold, jold, ii, jj);
+    framen = blurDnClr(framen, 3, binomialFilter(5));
+    
+    count = 1;
+    for r = 10:5:30
+        [rgbq(:,:,:,count), diffs(:,:,:,count)] = gradientAlongCircle(r, 200, framen, corner);
+        count = count + 1;
+    end
+    
+    %compute the average frame from all the circle differences
+    outframe(1,:,:) = mean(diffs,4); 
+    
+    %write out the video
+    outframe(outframe<minclip) = minclip;
+    outframe(outframe>maxclip) = maxclip; 
+    
+    
+    writeVideo(vout, (repmat(outframe, [100 1]) -minclip)./(maxclip-minclip)); 
 end
+
+close(vout); 
+
--- edgeImaging/edges/gradientAlongCircle.m
+++ edgeImaging/edges/gradientAlongCircle.m
@@ -9,14 +9,17 @@ for i = 1:size(rgbq,2)
     rgbq(:,i) = interp2(xx, yy, frame(:,:,i), xq, yq);
 end
 
-out = repmat(reshape(rgbq, [1, nsamples, 3]), [nsamples, 1, 1]);
-% figure; imagesc(uint8(out)); title(sprintf('r=%d stripe'));
 diffs = diff(rgbq);
-for i = 1:3
-    minv = min(diffs(:,i));
-    maxv = max(diffs(:,i));
-    diffs(:, i) = 2^8*(diffs(:, i) - minv)/(maxv - minv);
-end
-outdiffs = repmat(reshape(diffs, [1, nsamples-1, 3]), nsamples);
-figure; imagesc(uint8(outdiffs)); title(sprintf('r=%d gradient'));
+
+
+% out = repmat(reshape(rgbq, [1, nsamples, 3]), [nsamples, 1, 1]);
+% % figure; imagesc(uint8(out)); title(sprintf('r=%d stripe'));
+% diffs = diff(rgbq);
+% for i = 1:3
+%     minv = min(diffs(:,i));
+%     maxv = max(diffs(:,i));
+%     diffs(:, i) = 2^8*(diffs(:, i) - minv)/(maxv - minv);
+% end
+% outdiffs = repmat(reshape(diffs, [1, nsamples-1, 3]), nsamples);
+% figure; imagesc(uint8(outdiffs)); title(sprintf('r=%d gradient'));
 end

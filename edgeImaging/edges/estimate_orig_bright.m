064d7ef9154cae1f95a45a7fcf8921157a642e42
Vickie Ye <vye1116@gmail.com>
2017-01-20 19:47:11 -0500

    made separate script for monitor experiments

---
+++ edgeImaging/edges/estimate_orig_bright.m
@@ -0,0 +1,85 @@
+addpath(genpath('../utils/pyr'));
+addpath(genpath('rectify'));
+
+
+datafolder = '../data/testvideos/experiment_2';
+% datafolder = '/Users/klbouman/Downloads';
+gridfile = sprintf('%s/calibrationgrid.MOV', datafolder);
+backfile = sprintf('%s/dark_calibration.MOV', datafolder);
+moviefile = sprintf('%s/dark_MovieLines_greenred1.MOV', datafolder);
+outfile = sprintf('%s/out_dark_greenred_400_0.MOV', datafolder);
+nsamples = 400;
+start = 1;
+do_rectify = 0;
+downlevs = 3;
+
+readall = 1;
+
+if readall == 0
+    v = VideoReader(backfile);
+    background = double(read(v, 1));
+
+    v = VideoReader(moviefile);
+    nframes = v.NumberOfFrames;
+    frame1 = double(read(v,start)) - background;
+
+    if do_rectify == 1
+        vcali = VideoReader(gridfile);
+        caliImg = read(vcali,100);
+        [iold, jold, ii, jj, rectified_img] = rectify_image_solve(caliImg);
+        %figure;imagesc(rectified_img./max(rectified_img(:)))
+        frame1 = rectify_image(frame1, iold, jold, ii, jj);
+    end
+
+    frame1 = blurDnClr(frame1, downlevs, binomialFilter(5));
+    % frame1 = imresize(frame1, 0.5^downlevs);
+    imagesc(frame1(:,:,1));
+
+    corner = ginput(1);
+    hold on; plot(corner(1), corner(2), 'ro');
+    maxr = min(size(frame1, 2) - corner(1), size(frame1, 1) - corner(2)) - 1;
+end
+
+minclip = 0;
+maxclip = 1;
+step = 10;
+frames = zeros([1 + floor((nframes - start)/step), nsamples-1, 3]);
+vout = VideoWriter(outfile);
+vout.FrameRate = 10;
+open(vout);
+clear outframe rgbq diffs
+for n=start:step:800
+    n
+    
+    % read the nth frame
+    framen = double(read(v,n)) - background;
+    if do_rectify == 1
+        framen = rectify_image(framen, iold, jold, ii, jj);
+    end
+    framen = blurDnClr(framen, downlevs, binomialFilter(5));
+%     imagesc(framen(:,:,1));
+    
+    rs = 10:2:30;
+    angles = linspace(0, pi/2, 100);
+    for i = 1:length(rs)
+%         hold on; plot(corner(1) + rs(i) * cos(angles), corner(2) + rs(i) * sin(angles));
+        [rgbq(:,:,:,i), diffs(:,:,:,i)] = gradientAlongCircle(framen, corner, rs(i), nsamples);
+    end
+    
+    %compute the average frame from all the circle differences
+    outframe(1,:,:) = mean(diffs, 4);
+%     frames(n,:,:) = mean(diffs, 4);
+%     sum(frames(:))
+    
+%     diffs = sectorAverageGradient(framen, corner, nsamples, maxr);
+%     [pixel_avg, diffs] = estimatedGradient(framen, corner, nsamples, 10, 30);
+%     outframe(1,:,:) = diffs;
+    
+    %write out the video
+    outframe(outframe<minclip) = minclip;
+    outframe(outframe>maxclip) = maxclip;
+    writeVideo(vout, (repmat(outframe, [nsamples/2, 1]) -minclip)./(maxclip-minclip));
+end
+
+close(vout);
+

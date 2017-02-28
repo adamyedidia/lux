1ae5a2787aa0e87965cc23ed9c28fc1171783da9
Vickie Ye <vye@jones.csail.mit.edu>
2017-02-27 19:09:46 -0500

    small bug fix to saving out mean images script

--- edgeImaging/experiments/stereo/2017-02-20/save_all_mean_images.m
+++ edgeImaging/experiments/stereo/2017-02-20/save_all_mean_images.m
@@ -6,7 +6,8 @@ all_files = dir(datafolder);
 
 for i = 3:length(all_files)
     vid = all_files(i).name;
-    if ~strcmp(vid(1:4), 'cali')
+    if ~(strcmp(vid(1:4), 'cali') || strcmp(vid(1:6), 'messup'))...
+        && strcmp(vid(end-2:end), 'MP4')
         fname = fullfile(datafolder, vid);
         fprintf('computing mean for %s\n', fname);
         saveMeanImage(0, fname);

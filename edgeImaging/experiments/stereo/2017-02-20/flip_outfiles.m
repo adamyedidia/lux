7e752616c3b5775a29fc219eb32c9f5b2f2b2273
Vickie Ye <vye1116@gmail.com>
2017-03-01 13:48:45 -0500

    flipping outfiles if necessary

--- edgeImaging/experiments/stereo/2017-02-20/flip_outfiles.m
+++ edgeImaging/experiments/stereo/2017-02-20/flip_outfiles.m
@@ -1,18 +1,21 @@
 
-% resfolder = '/data/vision/billf/shadowImaging/edgeImaging/data/stereoDoor_Feb20/results';
-resfolder  = '/Users/vickieye/Dropbox (MIT)/shadowImaging/edgeImaging/data/stereodoor_Feb20/results';
+% myfolder = '/data/vision/billf/shadowImaging/edgeImaging/data/stereoDoor_Feb20/results';
+myfolder  = '/Users/vickieye/Dropbox (MIT)/shadowImaging/edgeImaging/data/stereodoor_Feb20/results';
 
-all_files = dir(resfolder);
+all_files = dir(myfolder);
 
 
 for i = 1:length(all_files)
-    fname = fullfile(resfolder, all_files(i).name);
+    fname = fullfile(myfolder, all_files(i).name);
     [path, name, ext] = fileparts(fname);
     if ~strcmp(ext, '.mat')
         continue
     end
-    load(fname);
-    name
+    load(fname, 'left_floor', 'left_scene',...
+        'right_floor', 'right_scene',...
+        'left_angles', 'left_floor_angles',...
+        'right_angles', 'right_floor_angles',...
+        'left_scene_angles', 'right_scene_angles');
     
     if sign(left_angles(2) - left_angles(1)) ~=...
             sign(left_floor_angles(2) - left_floor_angles(1))
@@ -25,15 +28,19 @@ for i = 1:length(all_files)
         right_scene = fliplr(right_scene);
     end
             
-    fname = strcat('left_floor', name(4:end), '.png')
-    imwrite((left_floor+0.05)/0.1, fullfile(resfolder, fname));
+    fname = strcat('left_floor', name(4:end), '.png');
+    imwrite((left_floor+0.05)/0.1, fullfile(myfolder, fname));
+    fprintf('imwrite to %s', fname);
 
-    fname = strcat('left_scene', name(4:end), '.png')
-    imwrite((left_scene+0.05)/0.1, fullfile(resfolder, fname));
+    fname = strcat('left_scene', name(4:end), '.png');
+    imwrite((left_scene+0.05)/0.1, fullfile(myfolder, fname));
+    fprintf('imwrite to %s', fname);
 
-    fname = strcat('right_floor', name(4:end), '.png')
-    imwrite((right_floor+0.05)/0.1, fullfile(resfolder, fname));
+    fname = strcat('right_floor', name(4:end), '.png');
+    imwrite((right_floor+0.05)/0.1, fullfile(myfolder, fname));
+    fprintf('imwrite to %s', fname);
 
-    fname = strcat('right_scene', name(4:end), '.png')
-    imwrite((right_scene+0.05)/0.1, fullfile(resfolder, fname));
+    fname = strcat('right_scene', name(4:end), '.png');
+    imwrite((right_scene+0.05)/0.1, fullfile(myfolder, fname));
+    fprintf('imwrite to %s', fname);
 end
